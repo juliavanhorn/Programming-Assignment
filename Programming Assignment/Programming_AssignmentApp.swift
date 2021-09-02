598f95f014f8e40715239d54f6aed647c4eae626
zsiwik8 <81332124+zsiwik8@users.noreply.github.com>
2021-09-02 17:26:59 -0400

    Initial Commit

---
+++ Programming Assignment/Programming_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_AssignmentApp.swift
+//  Programming Assignment
+//
+//  Created by zach siwik on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Programming_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
